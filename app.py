f9709e86c841a533b0f7eb50ec75b6d631302050
Divyanshi <147277838+Divyanshi-8953@users.noreply.github.com>
2025-09-01 00:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,23 +81,30 @@ if uploaded_file:
 
     st.image(query_path, caption="Query Image", width=200)
 
-    results = search_similar(query_path, product_db, top_k=5)
+    st.markdown(
+    """
+    <style>
+    div[data-testid="stHorizontalBlock"] {
+        justify-content: flex-start;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
 
+    results = search_similar(query_path, product_db, top_k=5)
+    cols = st.columns(len(results)) 
     st.subheader("Top Matches:")
-    html_code = ""
-    for match, score in results:
-        base = os.path.splitext(match["file"])[0]
-        for ext in [".jpg", ".jpeg", ".png"]:
-            img_path = os.path.join("images", base + ext)
-            if os.path.exists(img_path):
-                html_code += f """
-                    <div style="display:inline-block; margin-right:10px; text-align:center;">
-                        <img src="{img_path}" width="150"><br>
-                        {match['file']} Score={score:.3f}
-                    </div>
-                """
-                break
-    st.markdown(html_code, unsafe_allow_html=True)
+    
+    for i, (match, score) in enumerate(results):
+        with cols[i]:
+            base = os.path.splitext(match["file"])[0]
+            for ext in [".jpg", ".jpeg", ".png"]:
+                img_path = os.path.join("images", base + ext)
+                if os.path.exists(img_path):
+                    st.image(img_path, caption=f"{match['file']}\nScore={score:.3f}")
+                    break
+
 
 
 
